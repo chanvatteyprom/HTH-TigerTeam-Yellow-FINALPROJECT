ce4b8fcbe8597f9a47ee9803967d32c12a553b7f
Uchechi Mbakogu <215890943+uchechimbakogu@users.noreply.github.com>
2025-07-30 12:01:31 -0700

    final touches

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const nwbbutton = document.querySelector("#nwb");
-
 nwbbutton.addEventListener("click", nwbFunc);
-
 function nwbFunc() {
 document.getElementById("NotableWomenHeader").scrollIntoView({behavior: 'smooth'});
 }
 
 const aboutButton = document.querySelector("#about");
-
 aboutButton.addEventListener("click", aboutFunc);
 function aboutFunc() {
 document.getElementById("AboutUsHeader").scrollIntoView({behavior: 'smooth'});
 }
 
+const solutionsButton = document.querySelector("#solutions");
+solutionsButton.addEventListener("click", solutionsFunc);
+function solutionsFunc() {
+document.getElementById("SolutionsHeader").scrollIntoView({behavior: 'smooth'});
+}
+
+const gpgButton = document.querySelector("#gpg");
+gpgButton.addEventListener("click", gpgFunc);
+function gpgFunc() {
+document.getElementById("GenderPayGapHeader").scrollIntoView({behavior: 'smooth'});
+}
+
+const modeButton = document.getElementById("modeButton")
+modeButton.addEventListener("click", modeFunc);
+function modeFunc() { 
+    const bodyColor = document.body;
+    if (bodyColor.classList.contains("bodyLight"))
+         {bodyColor.classList.remove("bodyLight");
+            bodyColor.classList.add("bodyDark");
+         }
+         else {bodyColor.classList.remove("bodyDark");
+            bodyColor.classList.add("bodyLight");}
+document.getElementById("modeButton").classList.contains("bodyLight");}
